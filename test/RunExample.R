b63dcdf2a503f00be700f7fb647b0d12ccf0b445
Steve Hastings <hastings@nceas.ucsb.edu>
2013-11-13 13:26:05 -0800

    Reworked Parse function to use objects, (as ref-classes)

--- test/RunExample.R
+++ test/RunExample.R
@@ -1,42 +1,31 @@
 rm(list=ls())
 
-source('../R/ParseLayersNavigation.R')
+
+source('../R/SelectLayers.R')
 source('../R/ParseLayers.R')
 
-source('../R/CalculateResilienceComponent.R')
-
-source('../R/ParseSpatialSchemesNavigation.R')
+source('../R/TransformSpatialScheme.R')
 source('../R/ParseSpatialSchemes.R')
 
+source('../R/CalculateStatusComponent.R')
+source('../R/CalculatePressuresComponent.R')
+source('../R/CalculateResilienceComponent.R')
+
+source('../R/CalculateSubgoal.R')
+
+source('../R/Halpern2012Data.R')
 
 
-source('../R/calc.Subgoal.R')
-source('../R/calc.Goal.R')
-source('../R/ConvertSpatialScheme.R')
-source('../R/Halpern2012.R')
-source('../R/subcalc.Pressures.R')
-source('../R/subcalc.Status.R')
 
 
+layers.list <- Layers('data/fullpath.layers.navigation.csv')
+schemes.list <- SpatialSchemes('data/schemes_navigation.csv')
 
 
-layers.navigation <- ParseLayersNavigation('data/mod_layers_navigation.csv')
-layers.navigation$filepath <- gsub('(.*)', 'data/layers/\\1', layers.navigation$filepath)
-layers.list <- ParseLayers(layers.navigation)
+data = SelectLayers(layers.list, mode='target', target='FIS', cast=T)
+names(data) = gsub('country_id', 'country', names(data))
+
+data$region = NA
 
 
-focus <- layers.list[layers.list$target %in% c('HAB'), ]
-focus.df <- reshape2::dcast(plyr::rbind.fill(focus$layer.data),
-                           region_id ~ layer_id,
-                           value.var = 'value')
-
-
-tmp <- ParseSpatialSchemesNavigation('data/schemes_navigation.csv')
-tmp2 <- ParseSpatialSchemes(tmp)
-
-
-print (head(focus.df))
-print (focus[, -7])
-
-
-print (tmp2)
+tmp = TransformSpatialScheme(schemes.list, data, 'region', c('country'), c('i_fis_bt', 'i_fis_mmsy','i_fis_tc'))
