8700b67b77b801ceb250876f3efaf64a7abffac3
Ben Best <bdbest@gmail.com>
2013-12-03 13:00:39 -0800

    in process with generating layers.Global[2012|2013].v2013web

--- inst/extdata/data_create.R
+++ inst/extdata/data_create.R
@@ -2,6 +2,45 @@
 
 library(ohicore)
 
+# layers.Global[2013|2012].v2013web ----
+
+# set from root directory based on operating system
+dir.from.root = c('Windows' = '//neptune/data_edit',
+                  'Darwin'  = '/Volumes/data_edit',
+                  'Linux'   = '/var/data/ohi')[[ Sys.info()[['sysname']] ]]
+
+# load Google spreadsheet
+library(RCurl)
+g.url = 'https://docs.google.com/spreadsheet/pub?key=0At9FvPajGTwJdEJBeXlFU2ladkR6RHNvbldKQjhiRlE&output=csv'
+g = subset(read.csv(textConnection(getURL(g.url)), skip=1, na.strings=''), !is.na(ingest))
+write.csv(g, 'inst/extdata/layers.Global.vWeb2013b.csv', row.names=F, na='')
+
+# iterate over scenarios
+for (s in 2:3){ # s=2
+  cat(sprintf('\n---------\nScenario: 201%sa\n', s))  
+  
+  # copy files
+  dir.to     = sprintf('inst/extdata/layers.Global201%d.vWeb201%db', s, s)
+  dir.create(dir.to)
+  cat(sprintf('  copy to %s\n', dir.to))  
+  g$path = file.path(dir.from.root, g[, sprintf('dir_201%da',s)], g[, sprintf('fn_201%da' ,s)])
+  for (f in sort(g$path)){ # f = sort(g$path)[1]
+    cat(sprintf('    copying %s\n', f))
+    stopifnot(file.copy(f, file.path(dir.to, basename(f)), overwrite=T))
+  }
+  
+  # generate layers navigation csv  
+  layers.csv = sprintf('%s.csv', basename(dir.to))
+  write.csv(g[,c('target','layer','title','subtitle','citation','units','filename','directory')], layers_navigation.csv, row.names=F, na='')
+  
+  #check.layers_navigation(layers_navigation.csv, layers_id_fields)
+  #system(paste('open', layers_navigation.csv))
+  #assemble.layers_data(layers_navigation.csv, layers_data.csv, layers_id_fields)
+}
+
+# layers.Global2012.v2012Nature ----
+
+
 # layers.* ----
 # 
 # Create layers.[scenario] dataset for all layers.[scenario].csv based on csv
