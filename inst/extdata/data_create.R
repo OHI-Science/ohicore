d97c56e97e1c9749c3917ff6e9f3a2a433ce7fe3
Ben Best <bdbest@gmail.com>
2013-12-06 21:13:11 -0800

    finishing file commit for layers.Global[2012|2013].www2013

--- inst/extdata/data_create.R
+++ inst/extdata/data_create.R
@@ -7,9 +7,9 @@ dir.from.root = c('Windows' = '//neptune/data_edit',
                   'Darwin'  = '/Volumes/data_edit',
                   'Linux'   = '/var/data/ohi')[[ Sys.info()[['sysname']] ]]
 
-# # load Google spreadsheet
-# g.url = 'https://docs.google.com/spreadsheet/pub?key=0At9FvPajGTwJdEJBeXlFU2ladkR6RHNvbldKQjhiRlE&output=csv'
-# g = subset(read.csv(textConnection(RCurl::getURL(g.url)), skip=1, na.strings=''), !is.na(ingest))
+# load Google spreadsheet
+g.url = 'https://docs.google.com/spreadsheet/pub?key=0At9FvPajGTwJdEJBeXlFU2ladkR6RHNvbldKQjhiRlE&output=csv'
+g = subset(read.csv(textConnection(RCurl::getURL(g.url)), skip=1, na.strings=''), !is.na(ingest))
 
 # load results
 results.csv = '/Volumes/local_edit/src/toolbox/scenarios/global_2013a/results/OHI_results_for_Radical_2013-10-08.csv'
@@ -21,23 +21,23 @@ r$dimension = plyr::revalue(r$dimension, c('likely_future_state'='future'))
 for (yr in 2012:2013){ # yr=2012
   cat(sprintf('\n---------\nScenario: %da\n', yr))
   
-#   # copy files
-#   dir.to     = sprintf('inst/extdata/layers.Global%d.www2013', yr)
-#   dir.create(dir.to, showWarnings=F)
-#   cat(sprintf('  copy to %s\n', dir.to))
-#   g$filename = g[, sprintf('fn_%da' , yr)]
-#   g$path = file.path(dir.from.root, g[, sprintf('dir_%da', yr)], g$filename)
-#   for (f in sort(g$path)){ # f = sort(g$path)[1]
-#     cat(sprintf('    copying %s\n', f))
-#     stopifnot(file.copy(f, file.path(dir.to, basename(f)), overwrite=T))
-#   }
-#     
-#   # create conforming layers navigation csv  
-#   layers.csv = sprintf('%s.csv', dir.to)
-#   g$targets = gsub('_', ' | ', as.character(g$target), fixed=T)
-#   g$description = g$citation.2012n
-#   g$citation = g$X  
-#   write.csv(g[,c('targets','layer','title','description','citation','units','filename')], layers.csv, row.names=F, na='')
+  # copy files
+  dir.to     = sprintf('inst/extdata/layers.Global%d.www2013', yr)
+  dir.create(dir.to, showWarnings=F)
+  cat(sprintf('  copy to %s\n', dir.to))
+  g$filename = g[, sprintf('fn_%da' , yr)]
+  g$path = file.path(dir.from.root, g[, sprintf('dir_%da', yr)], g$filename)
+  for (f in sort(g$path)){ # f = sort(g$path)[1]
+    cat(sprintf('    copying %s\n', f))
+    stopifnot(file.copy(f, file.path(dir.to, basename(f)), overwrite=T))
+  }
+    
+  # create conforming layers navigation csv  
+  layers.csv = sprintf('%s.csv', dir.to)
+  g$targets = gsub('_', ' | ', as.character(g$target), fixed=T)
+  g$description = g$citation.2012n
+  g$citation = g$X  
+  write.csv(g[,c('targets','layer','title','description','citation','units','filename')], layers.csv, row.names=F, na='')
   
   # create scores
   scores.csv = sprintf('inst/extdata/scores.Global%d.www2013.csv', yr)
@@ -50,23 +50,23 @@ for (yr in 2012:2013){ # yr=2012
 #assemble.layers_data(layers_navigation.csv, layers_data.csv, layers_id_fields)
 
 
-# # layers.* ----
-# # 
-# # Create layers.[scenario] dataset for all layers.[scenario].csv based on csv
-# # layer files in layers.[scenario].  See: R/Layers.R for expected formats.
-# for (csv in list.files('inst/extdata', pattern=glob2rx('layers.*.csv'), full.names=T)){ # csv = list.files('inst/extdata', pattern=glob2rx('layers.*.csv'), full.names=T)[1]
-#   
-#   # get directory and ensure exists
-#   dir = tools::file_path_sans_ext(csv)
-#   stopifnot(file.exists(dir))  
-#   layers = basename(dir)
-#   
-#   # get layers and assign same variable name as dataset
-#   assign(layers, ohicore::Layers(csv, dir))
-#   
-#   # save to data folder for lazy loading
-#   save(list=c(layers), file=sprintf('data/%s.rda', layers))
-# }
+# layers.* ----
+# 
+# Create layers.[scenario] dataset for all layers.[scenario].csv based on csv
+# layer files in layers.[scenario].  See: R/Layers.R for expected formats.
+for (csv in list.files('inst/extdata', pattern=glob2rx('layers.*.csv'), full.names=T)){ # csv = list.files('inst/extdata', pattern=glob2rx('layers.*.csv'), full.names=T)[1]
+  
+  # get directory and ensure exists
+  dir = tools::file_path_sans_ext(csv)
+  stopifnot(file.exists(dir))  
+  layers = basename(dir)
+  
+  # get layers and assign same variable name as dataset
+  assign(layers, ohicore::Layers(csv, dir))
+  
+  # save to data folder for lazy loading
+  save(list=c(layers), file=sprintf('data/%s.rda', layers))
+}
 
 # scores.* ----
 # 
