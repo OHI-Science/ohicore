c651657db3e42f4afe7f986b095f6a73effdc712
Ben Best <bbest@nceas.ucsb.edu>
2014-02-20 16:44:04 -0800

    updated ICO to use a new layer with Canada's range weighted COSEWIC extinction risk status values. Note that the range weights and COSEWIC status weights should be updated to real values (see inst/extdata/layers_Canada-CHONe2014.R). Then the scores should be calculated again using the OHI app.

---
+++ inst/extdata/layers_Canada-CHONe2014.R
@@ -0,0 +1,92 @@
+library(plyr)
+library(dplyr)
+
+# TODO: update the path to your location where this script is located
+wd = '/Users/bbest/GitHub_Mac/ohicore_Canada-CHONe/inst/extdata'
+setwd(wd)
+
+# get species 
+spp_ico = read.csv('layers.Canada-CHONe2014/ICO/iconic_species.csv', stringsAsFactors=F); head(spp_ico)
+spp_sara = read.csv('layers.Canada-CHONe2014/ICO/SARA_sp.csv', stringsAsFactors=F); head(spp_sara)
+
+# TODO: fix iconics not matching to sara
+anti_join(spp_ico, spp_sara, by='Scientific_name')
+#    Common_name          Scientific_name              Source
+# 1      Lobster       Homarus americanus Royal Canadian Mint
+# 2         Loon              Gavia immer Royal Canadian Mint
+# 3   Bald Eagle Haliaeetus leucocephalus Royal Canadian Mint
+# 4       Salmon                     <NA>                    
+# 5         Orca          Orcinus\xa0orca Royal Canadian Mint
+# 6 Leather star   Dermasterias imbricata Royal Canadian Mint
+
+# create a dummy weight for species ranges to enable convergence on a single species value for all of Canada
+spp_range_weights = data.frame(
+  Range        = unique(spp_sara$Range),
+  range_weight = 1)
+write.csv(spp_range_weights, 'layers.Canada-CHONe2014/ICO/spp_range_weights.csv', row.names=F)
+
+# TODO: alter weights to something sensible, like using proportional area for range weights and IUCN analogue for status weights
+
+# read in weights
+spp_range_weights = read.csv('layers.Canada-CHONe2014/ICO/spp_range_weights.csv', stringsAsFactors=F); head(spp_range_weights)
+
+# create weights for species category. Can't just use category because averaging multiple ranges
+paste(unique(spp_sara$COSEWIC_Status), collapse="'=0.,'")
+spp_status_weights = c('Non-active'      = 0,
+                       'Data Deficient'  = 0,
+                       'Not at Risk'     = 0,
+                       'Special Concern' = 0.2,
+                       'Threatened'      = 0.4,
+                       'Endangered'      = 0.6,
+                       'Extinct'         = 1,
+                       'Extirpated'      = 1)
+spp_status_weights = data.frame(COSEWIC_Status = names(spp_status_weights),
+                                status_weight  = spp_status_weights, stringsAsFactors=F)
+
+# get status for iconics
+spp_ico_sara = spp_ico %.%
+  inner_join(spp_sara, by='Scientific_name') %.%
+  inner_join(spp_range_weights, by='Range') %.%
+  inner_join(spp_status_weights, by='COSEWIC_Status') %.%
+  rename(c(Scientific_name='sciname')) %.%
+  group_by(sciname) %.%
+  summarise(
+    value = weighted.mean(x=status_weight, w=range_weight),
+    rgn_id        = 118)
+  
+###
+# create new ico_spp_extinction_status now with extinction risk as numeric value and not categorical with these Canada values swapped in
+ico_spp_status = read.csv('layers.Global2013.www2013/ico_spp_extinction_status.csv', stringsAsFactors=F); head(ico_spp_extinction_status)
+
+# lookup for weights status
+w.risk_category = c('LC' = 0,
+                    'NT' = 0.2,
+                    'VU' = 0.4,
+                    'EN' = 0.6,
+                    'CR' = 0.8,
+                    'EX' = 1)
+# set value
+ico_spp_status$value = w.risk_category[ico_spp_status$category]; head(ico_spp_status)
+
+# inject Canada values
+ico_spp_status = ico_spp_status %.%
+  filter(rgn_id != 118) %.%
+  select(rgn_id, sciname, value) %.%
+  rbind(spp_ico_sara)
+  
+# write out new layer
+write.csv(ico_spp_status, 'layers.Global2013.www2013/ico_spp_extinction_status_value_Canada-CHONe.csv', na='', row.names=F)
+
+# read layers.csv
+layers = read.csv('layers.Global2013.www2013.csv', na.strings='', stringsAsFactors=F); head(layers); 
+
+# alter fields for this updated layer
+i = which(layers$layer=='rnk_ico_spp_extinction_status')
+layers$layer[i]     = 'rnk_ico_spp_extinction_status_value'
+layers$name[i]      = 'IUCN extinction risk value for iconic species'
+layers$units[i]     = 'value'
+layers$filename[i]  = 'ico_spp_extinction_status_value_Canada-CHONe.csv'
+layers$fld_value[i] = 'value'
+
+# write back updated layers.csv
+write.csv(layers, 'layers.Global2013.www2013.csv', na='', row.names=F)
