7b6c4453d0e13347435a10943cb707207c0df4ca
Remi_D <remi.daigle@dal.ca>
2014-02-25 17:13:18 -0400

    new folders/files to match hardcoded in ohigui

--- inst/extdata/scenario.R
+++ inst/extdata/scenario.R
@@ -2,8 +2,8 @@ require(ohicore)
 
 wd = "~/Documents/ohigit/ohicore/inst/extdata"
 scenario=list(
-  conf   = ohicore::Conf(file.path(wd, "conf.Canada-CHONe2014")),
-  layers = ohicore::Layers(file.path(wd, "layers.Canada-CHONe2014.csv"), file.path(wd, "layers.Canada-CHONe2014")),
-  scores = read.csv(file.path(wd, "scores.Canada-CHONe2014.csv"), na.strings=""),
-  spatial = file.path(wd, "spatial.Canada-CHONe2014"),
+  conf   = ohicore::Conf(file.path(wd, "conf")),
+  layers = ohicore::Layers(file.path(wd, "layers.csv"), file.path(wd, "layers")),
+  scores = read.csv(file.path(wd, "scores.csv"), na.strings=""),
+  spatial = file.path(wd, "spatial"),
   dir    = wd)
--- launchApp_code.R
+++ launchApp_code.R
@@ -4,10 +4,21 @@ require(ohigui)
 setwd('~/Documents/ohigit/ohicore/inst/extdata')
 source("layers_Canada-CHONE2014.R")
 
+# create new layers.csv, and layers folder for hard coded app ####################
+file.copy('layers.Canada-CHONe2014.csv', 'layers.csv', overwrite = T)
+file.copy('scores.Canada-CHONe2014.csv', 'scores.csv', overwrite = T)
+fl=list.files('layers.Canada-CHONe2014')
+file.copy(paste('layers.Canada-CHONe2014/',fl,sep = ""),paste('layers/',fl,sep = ""), overwrite = T)
+fl=list.files('conf.Canada-CHONe2014')
+file.copy(paste('conf.Canada-CHONe2014/',fl,sep = ""),paste('conf/',fl,sep = ""), overwrite = T)
+fl=list.files('spatial.Canada-CHONe2014')
+file.copy(paste('spatial.Canada-CHONe2014/',fl,sep = ""),paste('spatial/',fl,sep = ""), overwrite = T)
+
 wd = getwd()
 ohigui::launchApp(scenario=list(
-  conf   = ohicore::Conf("conf.Canada-CHONe2014"),
-  layers = ohicore::Layers("layers.Canada-CHONe2014.csv", "layers.Canada-CHONe2014"),
-  scores = read.csv("scores.Canada-CHONe2014.csv", na.strings=""),
-  spatial = file.path(wd, "spatial.Canada-CHONe2014"),
+  conf   = ohicore::Conf("conf"),
+  layers = ohicore::Layers("layers.csv", "layers"),
+  scores = read.csv("scores.csv", na.strings=""),
+  spatial = file.path(wd, "spatial"),
   dir    = wd), launch.browser=T)
+
