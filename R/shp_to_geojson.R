f6a91f3f22d1287204b29ef91db9f1898f3ba515
bbest <bbest@nceas.ucsb.edu>
2014-10-22 13:53:39 -0700

    swapped maptools for readOGR

--- R/shp_to_geojson.R
+++ R/shp_to_geojson.R
@@ -14,7 +14,6 @@ shp_to_geojson = function(shp, js, geojson=sprintf('%s.geojson', tools::file_pat
   
   require(sp)
   require(rgdal)
-  require(maptools)
   require(dplyr)
 
   # # debug
@@ -22,14 +21,28 @@ shp_to_geojson = function(shp, js, geojson=sprintf('%s.geojson', tools::file_pat
   # js      = '/Volumes/data_edit/git-annex/clip-n-ship/data/Albania/regions_gcs.js'
   # geojson = sprintf('%s.geojson', tools::file_path_sans_ext(js))
 
-  # read and write shapefile
-  x = maptools::readShapeSpatial(shp, proj4string=sp::CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'))
+  # vars
   flds = c('rgn_id'='rgn_id', 'rgn_name'='rgn_nam') #flds = c('rgn_id'='region_id', 'rgn_nam'='region_nam') # too long for shapefile columns
+  
+  # check shp exists
+  stopifnot(file.exists(shp))
+  
+  # read shp
+  lyr = tools::file_path_sans_ext(basename(f_shp))  
+  x = readOGR(dsn=shp, layer=lyr) #  proj4string=sp::CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+  
+  # check fields present
   stopifnot(names(flds) %in% names(x))
-  x@data = plyr::rename(x@data[,names(flds)], flds) # drop other fields  
+  
+  # drop other fields
+  x@data = plyr::rename(x@data[,names(flds)], flds)
+  
+  # write geojson
   if (file.exists(geojson)) unlink(geojson)
-  rgdal::writeOGR(x, dsn=tools::file_path_sans_ext(geojson), layer=basename(tools::file_path_sans_ext(geojson)), driver='GeoJSON')
+  writeOGR(x, dsn=tools::file_path_sans_ext(geojson), layer=basename(tools::file_path_sans_ext(geojson)), driver='GeoJSON')  
   file.rename(tools::file_path_sans_ext(geojson), geojson)
+  
+  # write javascript
   fw = file(js, 'wb')
   cat('var regions = ', file=fw)
   cat(readLines(geojson, n = -1), file=fw)
