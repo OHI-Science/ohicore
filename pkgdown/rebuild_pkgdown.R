e193aebc8ccffd250415b60b732eff0967347197
eleanorecc <eleanorecampbell@ucsb.edu>
2018-09-10 12:59:55 -0700

    small revisions to vignettes

--- pkgdown/rebuild_pkgdown.R
+++ pkgdown/rebuild_pkgdown.R
@@ -6,9 +6,16 @@ library(pkgdown) # install.packages("pkgdown")
 
 ## check that these reflects correct repo links...
 ## any ohi code links external to the package/repo used in vignettes etc, specify here
-demo_folder <- "https://github.com/OHI-Science/ohicore/tree/dev/pkgdown/toolbox-demo"
-demo_functions <- "https://github.com/OHI-Science/ohicore/blob/dev/pkgdown/toolbox-demo/conf/functions.R"
-demo_calculate_scores <- "https://github.com/OHI-Science/ohicore/blob/dev/pkgdown/toolbox-demo/calculate_scores.R"
+
+demo_folder <- "https://github.com/OHI-Science/ohicore/blob/dev/pkgdown/demo" # "https://github.com/OHI-Science/ohicore/blob/master/pkgdown/demo"
+
+demo_functions <- paste(demo_folder, "conf/functions.R", sep = "/")
+demo_calculate_scores <-  paste(demo_folder, "calculate_scores.R", sep = "/")
+demo_config_toolbox <-  paste(demo_folder, "configure_toolbox.R", sep = "/")
+
+demo_layers_csv <- paste(demo_folder, "layers.csv", sep = "/")
+demo_layers_csv <- paste(demo_folder, "referencePoints.csv", sep = "/")
+
 
 ## build
 devtools::load_all()
